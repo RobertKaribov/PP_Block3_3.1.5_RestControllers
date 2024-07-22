6d85b5500a32330e09c77c98ab4343316f65ef97
RobertKaribov <infobox.markets@gmail.com>
2024-07-23 01:22:48 +0300

    1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,63 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.util.List;
+
+
+@Controller
+public class AdminController {
+
+    private final UserService userService;
+
+    private final RoleService roleService;
+
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping("/admin")
+    public String showAllUsers(ModelMap modelMap) {
+        List<User> users = userService.showAllUsers();
+       modelMap.addAttribute("users", users);
+        return "admin";
+    }
+
+    @PostMapping(value = "/admin/addUser")
+    public String addNewUser(@RequestParam("role") String role, @ModelAttribute("user") User user) {
+        userService.addNewUser(role,user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/updateUser")
+    public String editUserForm(@RequestParam("id") Long id, ModelMap modelMap) {
+        User userUpdate = userService.findById(id).orElse(null);
+        List<Role> roles = roleService.findAll();
+        modelMap.addAttribute("user", userUpdate);
+        modelMap.addAttribute("listRoles", roles);
+        return "admin";
+    }
+
+    @PostMapping(value = "/admin/updateUser")
+    public String updateUser(@RequestParam("role") String role, @ModelAttribute("user") User user) {
+        userService.updateUser(role ,user);
+        return "redirect:/admin";
+    }
+
+    @PostMapping(value = "/admin/deleteUser")
+    public String deleteUser(@RequestParam long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
